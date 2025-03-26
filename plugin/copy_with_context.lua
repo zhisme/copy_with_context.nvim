88f783d2cbda04064cd6a5d06e65ba12a06009cb
Evgeny Zhdanov <evdev34@gmail.com>
2025-03-27 00:06:09 +0300

    add ci conf (#15)

--- plugin/copy_with_context.lua
+++ plugin/copy_with_context.lua
@@ -2,9 +2,12 @@
 -- Author: Evgeny Zhdanov evdev34@gmail.com
 -- Description: Copy lines with file path and line number context
 
+-- luacheck: globals vim
 if vim.g.loaded_copy_with_context == 1 then
   return
 end
+
+-- luacheck: ignore 5 11
 vim.g.loaded_copy_with_context = 1
 
-require('copy_with_context').setup()
+require("copy_with_context").setup()
