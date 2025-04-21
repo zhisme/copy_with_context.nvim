7ccfe6a4a6845990b0bab6afcd83b2602413d031
Evgeny Zhdanov <evdev34@gmail.com>
2025-04-21 10:26:28 +0300

    fix spec

--- tests/copy_with_context/utils_spec.lua
+++ tests/copy_with_context/utils_spec.lua
@@ -105,19 +105,19 @@ describe("Utility Functions", function()
 
   describe("process_lines", function()
     local config_mock = {
-      options = { trim_lines = true },
+      options = { trim_lines = false },
     }
 
     before_each(function()
       package.loaded["copy_with_context.config"] = config_mock
     end)
 
-    it("trims lines if config option is enabled", function()
+    it("keeps lines unchanged if trim is disabled", function()
       local result = utils.process_lines({ "  hello  ", " world " })
-      assert.same({ "hello", "world" }, result)
+      assert.same({ "  hello  ", " world " }, result)
     end)
 
-    it("keeps lines unchanged if trim is disabled", function()
+    it("trims lines if config option is enabled", function()
       config_mock.options.trim_lines = true
       local result = utils.process_lines({ "  hello  ", " world " })
       assert.same({ "hello", "world" }, result)
